e344bc14496783f484492c323318a989bbe73511
Nestor Alonso <nestor.alonso.m@gmail.com>
2018-01-03 13:34:16 -0500

    Added delete and voting

--- client/src/posts/components/PostDetail.jsx
+++ client/src/posts/components/PostDetail.jsx
@@ -1,40 +1,130 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { getPost } from '../redux/selectors'
-import { fetchSinglePost } from '../redux/actions'
+import { fetchSinglePost, vote, deletePost, updatePost } from '../redux/actions'
 import CommentList from '../../comments/components/CommentList.jsx'
 import { formatDate } from '../../utils/formatDate'
-
+import { Redirect } from 'react-router-dom'
 import style from './PostDetail.css'
+import PostForm from './PostForm'
 
 class PostDetail extends Component {
+  state = {
+    editMode: false
+  }
+
   componentDidMount = () => {
     const { match: { params: { postId } = {} } = {} } = this.props
     this.props.fetchSinglePost(postId)
   }
 
+  renderVotingGroup = () => {
+    const { post: { voteScore } } = this.props
+    return (
+      <Fragment>
+        <div className={style.votes}>{voteScore} votes</div>
+        <div className="btn-group">
+          <button
+            className="btn btn-sm btn-light"
+            title="Vote up"
+            onClick={this.upVote}
+          >
+            <i className="fa fa-arrow-up" aria-hidden="true" />
+          </button>
+
+          <button
+            className="btn btn-sm btn-light"
+            title="Vote down"
+            onClick={this.downVote}
+          >
+            <i className="fa fa-arrow-down" aria-hidden="true" />
+          </button>
+        </div>
+      </Fragment>
+    )
+  }
+
+  upVote = () => {
+    const { post: { id } } = this.props
+    this.props.vote(id, 1)
+  }
+
+  downVote = () => {
+    const { post: { id } } = this.props
+    this.props.vote(id, -1)
+  }
+
+  delete = e => {
+    this.props.deletePost(this.props.post.id)
+  }
+
+  toggleEditMode = () => {
+    this.setState(({ editMode }) => {
+      return {
+        editMode: !editMode
+      }
+    })
+  }
+
+  renderPostDetail = () => {
+    const { post: { title, author, timestamp, body } = {} } = this.props
+
+    return (
+      <div className={style.postDetail}>
+        <div className={style.detailHeader}>
+          <h5>{title}</h5>
+          <div className={style.postToolbar}>
+            <button
+              className="btn btn-sm"
+              onClick={this.toggleEditMode}
+              title="edit"
+            >
+              <i className="fa fa-pencil" aria-hidden="true" />
+            </button>
+            <button className="btn btn-sm" onClick={this.delete} title="Delete">
+              <i className="fa fa-trash" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+        <div className={style.textSmall}>
+          By {author} on {formatDate(timestamp)}
+        </div>
+        <div className={style.votingGroup}>{this.renderVotingGroup()}</div>
+        <p className={style.textLarge}>{body}</p>
+      </div>
+    )
+  }
+
+  onEditFinished = newPost => {
+    const { post: oldPost } = this.props
+    const payload = { ...oldPost, ...newPost }
+    payload.timestamp = oldPost.timestamp
+    this.props.updatePost(payload)
+    this.toggleEditMode()
+  }
+
   render() {
     const { post } = this.props
-    const { title, author, voteScore, timestamp, body, id } = post || {}
+    const { id } = post || {}
+    const { match: { params: { categoryId } = {} } = {} } = this.props
 
     if (!id) {
       return (
-        <div className={style.postDetail}>
-          <h5>Post Not Found</h5>
-        </div>
+        <Redirect to={`/${categoryId}`} />
       )
     }
 
     return (
       <Fragment>
-        <div className={style.postDetail}>
-          <h5>{title}</h5>
-          <div className={style.textSmall}>
-            By {author} on {formatDate(timestamp)}
-          </div>
-          <div className={style.textSmall}>Votes: {voteScore}</div>
-          <p className={style.textLarge}>{body}</p>
-        </div>
+        {this.state.editMode ? (
+          <PostForm
+            post={post}
+            onCancel={this.toggleEditMode}
+            onEditFinished={this.onEditFinished}
+          />
+        ) : (
+          this.renderPostDetail()
+        )}
 
         <CommentList postId={id} />
       </Fragment>
@@ -48,7 +138,11 @@ const mapStateToProps = (state, ownProps) => {
     post
   }
 }
+
 const mapDispatchToProps = {
-  fetchSinglePost
+  fetchSinglePost,
+  vote,
+  deletePost,
+  updatePost
 }
 export default connect(mapStateToProps, mapDispatchToProps)(PostDetail)
